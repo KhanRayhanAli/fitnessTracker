3c7506d39402c694ce242ac40ac390dd52e75745
Andrew Crow <andrewccrow@gmail.com>
2022-11-08 11:04:35 -0600

    continuing to work on filling out functions

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -9,6 +9,16 @@ async function addActivityToRoutine({
   count,
   duration,
 }) {
+  try {
+    const {rows: [routineActivity]} = await client.query (`
+      INSERT INTO routine_activities ("routineId", "activityId", count, duration)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+    `, [routineId, activityId, count, duration])
+    return routineActivity
+  } catch (error) {
+    throw error
+  }
     
 }
 
--- db/routines.js
+++ db/routines.js
@@ -1,12 +1,42 @@
 const client = require('./client');
 
 async function getRoutineById(id){
+  try {
+  const {rows: [routine]} = await client.query (`
+    SELECT *
+    FROM routines
+    WHERE id= $1;
+  `, [id])
+    // console.log(routine, "i am routine")
+  return routine
+} catch (error) {
+  throw error
+}
+
 }
 
 async function getRoutinesWithoutActivities(){
+  try {
+    const {rows: [routine]} = await client.query (`
+      SELECT *
+      FROM routines
+    `)
+    return routine
+  } catch (error) {
+    throw (error)
+  }
 }
 
 async function getAllRoutines() {
+  try {
+    const {rows: [routine]} = await client.query (`
+      SELECT id
+      FROM routine_activities
+    `)
+    return routine
+  } catch (error) {
+    throw (error)
+  }
 }
 
 async function getAllRoutinesByUser({username}) {
@@ -22,6 +52,17 @@ async function getPublicRoutinesByActivity({id}) {
 }
 
 async function createRoutine({creatorId, isPublic, name, goal}) {
+
+  try {
+    const {rows: [routine]} = await client.query (`
+      INSERT INTO routines ("creatorId", "isPublic", name, goal)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+    `, [creatorId, isPublic, name, goal])
+    return routine
+  } catch (error) {
+    throw error
+  }
 }
 
 async function updateRoutine({id, ...fields}) {
