e3b58ff4fb53b4f573828e9ed5d55decbc2ecd5f
Andrew Crow <andrewccrow@gmail.com>
2022-11-10 10:11:40 -0600

    finishing building the routine activities functions

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -1,6 +1,16 @@
 const client = require('./client')
 
 async function getRoutineActivityById(id){
+  try{
+    const {rows:[routineActivity]} = await client.query (`
+      SELECT *
+      FROM routine_activities
+      WHERE id = $1
+    `, [id])
+    return routineActivity
+  }catch (error) {
+    throw error
+  }
 }
 
 async function addActivityToRoutine({
@@ -22,14 +32,56 @@ async function addActivityToRoutine({
   }
     
 }
-
+//Questions on how this function works and what it returns in terms of the ID's associated with it.
 async function getRoutineActivitiesByRoutine({id}) {
+  try {
+    const {rows} = await client.query (`
+      SELECT routine_activities.*
+      FROM routine_activities
+      JOIN routines ON routine_activities."routineId" = routines.id
+      WHERE routines.id = $1
+    `, [id])
+    return rows
+  } catch (error) {
+    throw error
+  }
 }
 
 async function updateRoutineActivity ({id, ...fields}) {
+ 
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${ key }"=$${ index + 1 }`
+  ).join(', ');
+
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const {rows: [ routineActivity ] } = await client.query(`
+    UPDATE routine_activities
+    SET ${setString}
+    WHERE id=${id}
+    RETURNING *;
+    `, Object.values(fields));
+
+    return routineActivity
+  }catch (error) {
+    throw error
+  }
 }
 
 async function destroyRoutineActivity(id) {
+  try{
+  const {rows: [routineActivity]} = await client.query (`
+    DELETE FROM routine_activities
+    WHERE id=$1
+    RETURNING *
+  `, [id])
+  return routineActivity
+  } catch (error) {
+    throw error
+  }
 }
 
 async function canEditRoutineActivity(routineActivityId, userId) {
