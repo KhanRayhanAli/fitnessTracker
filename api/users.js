02d8a777eac9e9f6ca30b61cb6e441ac176a69ac
rayhankhan <khanrayhanali@gmail.com>
2022-11-13 20:15:45 -0600

    users.api/login fix

--- api/users.js
+++ api/users.js
@@ -1,17 +1,19 @@
-const express = require('express');
+const express = require("express");
 const usersRouter = express.Router();
-const {getUserByUsername, createUser} = require('../db')
-const {getAllRoutinesByUser, getPublicRoutinesByUser} = require('../db/routines')
-const {requireUser} = require('./utils')
+const { getUserByUsername, createUser } = require("../db");
+const {
+  getAllRoutinesByUser,
+  getPublicRoutinesByUser,
+} = require("../db/routines");
+const { requireUser } = require("./utils");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = process.env;
 
-usersRouter.use((req,res, next) => {
+usersRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
 
-  next()
-})
-
+  next();
+});
 
 // POST /api/users/login
 
@@ -36,7 +38,7 @@ usersRouter.post("/login", async (req, res, next) => {
         { expiresIn: "1w" }
       );
 
-      res.send({ token, message: "you're logged in!" });
+      res.send({ token, user, message: "you're logged in!" });
     } else {
       next({
         name: "IncorrectCredentialsError",
@@ -44,87 +46,85 @@ usersRouter.post("/login", async (req, res, next) => {
       });
     }
   } catch (error) {
-    console.log(error, 'error1');
+    console.log(error, "error1");
     next(error);
   }
 });
 
 // POST /api/users/register
 
-usersRouter.post('/register', async (req, res, next) => {
-    const { username, password } = req.body;
-
-    try {
-
-    if (!username || !password) {
+usersRouter.post("/register", async (req, res, next) => {
+  const { username, password } = req.body;
+  if (password.length < 8) {
       next({
-        name: "MissingCredentialsError",
-        message: "Please supply both a username and password"
-      })
-    }
-
-    if(password.length < 8) {
-      next ({
-        name: 'PasswordLengthError', 
-        message: 'Password must be at least 8 characters'
+        name: "PasswordLengthError",
+        message: "Password Too Short!"
       });
     }
     
-      const _user = await getUserByUsername(username);
-      
-      if (_user) {
-        next({
-          name: 'UserExistsError',
-          message: `${username} is already taken. Please try a different one.`,
-          error: 'UserExistsError'
-        }); 
-      } else {
-        const user = await createUser({username, password
+  try {
+    if (!username || !password) {
+      next({
+        name: "MissingCredentialsError",
+        message: "Please supply both a username and password",
+      });
+    }
+
+  
+
+    const _user = await getUserByUsername(username);
+
+    if (_user) {
+      next({
+        name: "UserExistsError",
+        message: `${username} is already taken. Please try a different one.`,
+        error: "UserExistsError",
+      });
+    } else {
+      const user = await createUser({ username, password });
+
+      const token = jwt.sign({ id: user.id, username }, JWT_SECRET, {
+        expiresIn: "1w",
       });
 
-      const token = jwt.sign({id: user.id, username}, JWT_SECRET, { expiresIn: "1w",})
-
-      res.send({ 
+      res.send({
         message: "thank you for signing up",
         token,
-        user
-      })
+        user,
+      });
     }
-    } catch (error) {
-      next(error)
-    } 
-  });
+  } catch (error) {
+    next();
+  }
+});
 
 // GET /api/users/me
 
-usersRouter.get('/me', requireUser, async (req, res, next) => {
-  try{
-    res.send(req.user)
+usersRouter.get("/me", requireUser, async (req, res, next) => {
+  try {
+    res.send(req.user);
+  } catch (error) {
+    console.log(error, "error2");
+    next(error.name, error.message);
   }
-  catch(error) {
-    console.log(error, 'error2')
-    next(error);
-  }
-})
+});
 
 // GET /api/users/:username/routines
 
-usersRouter.get('/:username/routines', async (req, res, next) => {
+usersRouter.get("/:username/routines", async (req, res, next) => {
+  const { username } = req.params;
 
-  const { username } = req.params
-
-  try{
+  try {
     const allUsers = await getAllRoutinesByUser(username);
 
-    const routines = allUsers.filter(routine => {
-      return routine.active || (req.user && routine.author.id === req.user.id)
-    })
+    const routines = allUsers.filter((routine) => {
+      return routine.active || (req.user && routine.author.id === req.user.id);
+    });
 
-    res.send({routines: routines})
-  }
-  catch(error) {
+    res.send({ routines: routines });
+  } catch (error) {
     next(error);
   }
-})
+});
 
 module.exports = usersRouter;
