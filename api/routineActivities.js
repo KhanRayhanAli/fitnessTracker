245a3e609664c9d29dfca33b2025422acbe63f1e
lucasawest <lucasawest1@gmail.com>
2022-11-14 23:24:49 -0700

    one check on Delete routine activity

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { requireUser } = require('./utils');
-const { getRoutineActivityById, updateRoutineActivity } = require('../db')
+const { getRoutineActivityById, updateRoutineActivity, destroyRoutineActivity } = require('../db')
 const routineActivitiesRouter = express.Router();
 
 routineActivitiesRouter.use((req,res, next) => {
@@ -14,27 +14,34 @@ routineActivitiesRouter.use((req,res, next) => {
 // DELETE /api/routine_activities/:routineActivityId
 
 routineActivitiesRouter.delete('/:routineActivityId', requireUser, async (req, res, next) => {
-    try {
-      const routineActivity = await getRoutineActivityById({id:req.params.routineActivityId});
-  
-      if (routineActivity && routineActivity.author.id === req.user.id) {
-        const updatedRoutineActivity = await updateRoutineActivity(routineActivity.id, { active: false });
-  
-        res.send({ post: updatedRoutineActivity });
-      } else {
-        // if there was a routine activity, throw UnauthorizedUserError, otherwise throw RoutineActivityNotFoundError
-        next(routineActivity ? { 
-          name: "UnauthorizedUserError",
-          message: "You cannot delete a RoutineActivity which is not yours"
-        } : {
-          name: "RoutineActivityNotFoundError",
-          message: "That RoutineActivity does not exist"
-        });
-      }
-  
-    } catch ({ name, message }) {
-      next({ name, message })
-    }
+  try {
+    const {routineActivityId: id } = req.params
+    const routineActivity = await destroyRoutineActivity(id);
+    console.log('routineActivity', routineActivity)
+    res.send(routineActivity)
+    
+  } catch ({ name, message }) {
+    next({ name, message })
+  }
   });
+// routineActivitiesRouter.delete('/:routineActivityId', requireUser, async (req, res, next) => {
+//     try {
+//       const routineActivity = await getRoutineActivityById({id:req.params.routineActivityId});
+  
+//       if (routineActivity && routineActivity.author.id === req.user.id) {
+//         const updatedRoutineActivity = await updateRoutineActivity(routineActivity.id, { active: false });
+  
+//         res.send({ post: updatedRoutineActivity });
+//       } else {
+//         // if there was a routine activity, throw UnauthorizedUserError, otherwise throw RoutineActivityNotFoundError
+//         next(routineActivity ? { 
+//           name: "UnauthorizedUserError",
+//           message: "You cannot delete a RoutineActivity which is not yours"
+//         } : {
+//           name: "RoutineActivityNotFoundError",
+//           message: "That RoutineActivity does not exist"
+//         });
+//       }
+  
 
 module.exports = routineActivitiesRouter;
